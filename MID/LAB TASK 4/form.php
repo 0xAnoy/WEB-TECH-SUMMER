0c41f13bfaab257af7512f11b6c8989abce4ef3e
Iftier Ahmed Anoy <iftierahmed104@gmail.com>
2025-08-06 13:51:36 +0600

    FInal css

--- MID/LAB TASK 4/form.php
+++ MID/LAB TASK 4/form.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>AIUB Clinic</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -12,26 +13,32 @@
         
         <label>Full Name:</label>
         <input type="text" id="fullname"><br>
+        <span id="error-fullname" class="error"></span>
 
-        <label>Age:</label>
-        <input type="number" id-"age"><br>
+        <label>Age:</label><br>
+        <input type="number" id="age"><br>
+        <span id="error-age" class="error"></span>
 
         <label>Phone Number:</label>
         <input type="text" id="phone"><br>
+        <span id="error-phone" class="error"></span>
 
         <label>Email:</label>
         <input type="email" id="email"><br>
+        <span id="error-email" class="error"></span>
 
-        <label>Insurance Provider:</label>
+        <label>Insurance Provider:</label><br>
         <select id="insurance">
             <option>Select</option>
             <option>Metlife</option>
             <option>Green Delta</option>
             <option>City Insurance</option>
+            <span id="error-insurance" class="error"></span>
         </select><br>
 
         <label>Insurance Policy Number:</label>
         <input type="text" id="policy" pattern="[a-zA-Z0-9]"><br>
+        <span id="error-policy" class="error"></span>
         <h3>Additional Informatiom</h3>
 
         <label>Username:</label>
@@ -47,13 +54,3 @@
 
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
