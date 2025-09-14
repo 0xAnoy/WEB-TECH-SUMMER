db6a9f32592cc8247faaf8bf96182c4c38ef8371
0xAnoy <iftierahmed104@gmail.com>
2025-09-14 12:08:33 +0600

    REMOVE FILKES FROM MAIN DIRECTORY

--- FINAL/PROJECT/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-setcookie('remember_user_id','',time()-3600,'/');
-setcookie('remember_username','',time()-3600,'/');
-setcookie('remember_email','',time()-3600,'/');
-header('Location: login.php');
-exit;
